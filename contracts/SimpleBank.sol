2e9295b1fa5aa92935a0d06107bcdce9ca06db1d
Nagendra Kumar Y <ynkumar.nagendra@gmail.com>
2019-01-20 10:10:26 +0530

    Modified according to latest truffle and web3.js

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -1,4 +1,5 @@
-pragma solidity ^0.4.21;
+pragma solidity >=0.4.21 <0.6.0;
+
 contract SimpleBank {
 
     //
@@ -84,7 +85,7 @@ contract SimpleBank {
            Subtract the amount from the sender's balance, and try to send that amount of ether
            to the user attempting to withdraw. 
            return the user's balance.*/
-        address sender = msg.sender; 
+        address payable sender = msg.sender; 
         require(balances[sender] >= withdrawAmount);
         balances[sender] -= withdrawAmount; 
         sender.transfer(withdrawAmount);
@@ -97,7 +98,7 @@ contract SimpleBank {
     // Typically, called when invalid data is sent
     // Added so ether sent to this contract is reverted if the contract fails
     // otherwise, the sender's money is transferred to contract
-    function() public payable {
+    function() external payable {
         revert();
     }
 }
--- test/simpleBank.test.js
+++ test/simpleBank.test.js
@@ -5,7 +5,7 @@ contract('SimpleBank', function(accounts) {
   const owner = accounts[0]
   const alice = accounts[1];
   const bob = accounts[2];
-  const deposit = web3.toBigNumber(2);
+  const deposit = web3.utils.toBN(2);
 
   it("mark addresses as enrolled", async () => {
     const bank = await SimpleBank.deployed();
@@ -25,19 +25,19 @@ contract('SimpleBank', function(accounts) {
     await bank.enroll({from: alice});
     await bank.enroll({from: bob});
 
-    await bank.deposit({from: alice, value: deposit});
+    var result = await bank.deposit({from: alice, value: deposit});
     const balance = await bank.balance({from: alice});
     assert.equal(deposit.toString(), balance, 'deposit amount incorrect, check deposit method');
 
     const expectedEventResult = {accountAddress: alice, amount: deposit};
 
-    const LogDepositMade = await bank.LogDepositMade();
-    const log = await new Promise(function(resolve, reject) {
-        LogDepositMade.watch(function(error, log){ resolve(log);});
-    });
+    // const LogDepositMade = await bank.LogDepositMade();
+    // const log = await new Promise(function(resolve, reject) {
+    //     LogDepositMade.watch(function(error, log){ resolve(log);});
+    // });
 
-    const logAccountAddress = log.args.accountAddress;
-    const logDepositAmount = log.args.amount.toNumber();
+    const logAccountAddress = result.logs[0].args.accountAddress;
+    const logDepositAmount = result.logs[0].args.amount.toNumber();
 
     assert.equal(expectedEventResult.accountAddress, logAccountAddress, "LogDepositMade event accountAddress property not emitted, check deposit method");
     assert.equal(expectedEventResult.amount, logDepositAmount, "LogDepositMade event amount property not emitted, check deposit method");
@@ -47,19 +47,19 @@ contract('SimpleBank', function(accounts) {
     const bank = await SimpleBank.deployed();
     const initialAmount = 0;
    
-    await bank.withdraw(deposit, {from: alice});
+    var result = await bank.withdraw(deposit, {from: alice});
     const balance = await bank.balance({from: alice});
 
     assert.equal(balance.toString(), initialAmount.toString(), 'balance incorrect after withdrawal, check withdraw method');
 
-    const LogWithdrawal = await bank.LogWithdrawal();
-    const log = await new Promise(function(resolve, reject) {
-      LogWithdrawal.watch(function(error, log){ resolve(log);});
-    });
+    // const LogWithdrawal = await bank.LogWithdrawal();
+    // const log = await new Promise(function(resolve, reject) {
+    //   LogWithdrawal.watch(function(error, log){ resolve(log);});
+    // });
     
-    const accountAddress = log.args.accountAddress;
-    const newBalance = log.args.newBalance.toNumber();
-    const withdrawAmount = log.args.withdrawAmount.toNumber();
+    const accountAddress = result.logs[0].args.accountAddress;
+    const newBalance = result.logs[0].args.newBalance.toNumber();
+    const withdrawAmount = result.logs[0].args.withdrawAmount.toNumber();
 
     const expectedEventResult = {accountAddress: alice, newBalance: initialAmount, withdrawAmount: deposit};
 
